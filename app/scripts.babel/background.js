d47fecc44d178dc0f815afcd196fa9699fa3b1db
Tom Wall <twall@millennialmedia.com>
2016-09-10 15:32:55 -0400

    cleaning up

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({ text: '\'Allo' });
+chrome.browserAction.setBadgeText({ });
 
 
 let tabIdToPreviousUrl = {};
