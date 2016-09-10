55c6afa97b2672f6d57e66917dd2e96565902940
Tom Wall <twall@millennialmedia.com>
2016-09-10 15:27:30 -0400

    initial commit

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,35 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({ text: '\'Allo' });
+
+
+let tabIdToPreviousUrl = {};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status == 'complete') {
+        let previousUrl;
+        let currentUrl = {
+          url: tab.url,
+          time: new Date().getTime()
+        };
+
+        if (tabId == tab.id) {
+            previousUrl = tabIdToPreviousUrl[tabId];
+        }
+
+        axios.post('http://54.213.194.217:3000/site_visit', {previous: previousUrl, current: currentUrl})
+          .then(function (res) {
+            return console.log(res);
+          })
+          .catch(err => console.log);
+
+        tabIdToPreviousUrl[tabId] = currentUrl;
+
+    }
+});
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
